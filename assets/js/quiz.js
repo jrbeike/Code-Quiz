d4eba8e898ab7c6f76933da733746f20c71197f0
jrbeike <jrbeike@gmail.com>
2022-03-04 22:51:15 -0500

    Update quiz.js
    insert quiz qestions.

---
+++ assets/js/quiz.js
@@ -0,0 +1,53 @@
+/*Global variables */
+
+var score = 0;
+var correctAnswers = 0;
+var quiz = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        answer1: "strings",
+        answer2: "booleans",
+        answer3: "alerts",
+        answer4: "numbers",
+        correctAnswer: "alerts",
+
+    },
+    {
+        question: "The condition if an if / else statement is enclosed with _________.",
+        answer1: "quotes",
+        answer2: "curly brackets",
+        answer3: "parenthesis",
+        answer4: "square brackets",
+        correctAnswer: "parenthesis",
+
+    },
+    {
+        question: "Arrays in JavaScript can be used store ________.",
+        answer1: "numbers and strings",
+        answer2: "other arrays",
+        answer3: "booleans",
+        answer4: "all of the above",
+        correctAnswer: "all of the above",
+
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answer1: "commas",
+        answer2: "curly brackets",
+        answer3: "quotes",
+        answer4: "parenthesis",
+        correctAnswer: "quotes",
+
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        answer1: "Javascript",
+        answer2: "terminal/bash",
+        answer3: "for loops",
+        answer4: "console.log()",
+        correctAnswer: "console.log()",
+
+    },
+];
+
+/**** */
